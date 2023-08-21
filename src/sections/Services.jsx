7c68bdd6bca22c166c1b2587187b0138665f8806
swoyamshk <swoyam301@gmail.com>
2023-08-21 20:05:38 +0545

    first commit

---
+++ src/sections/Services.jsx
@@ -0,0 +1,14 @@
+import { services } from "../constants"
+import ServiceCard from "../components/ServiceCard"
+
+const Services = () => {
+  return (
+    <section className="max-container justify-center flex flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service}/>
+      ))}
+       </section>
+  )
+}
+
+export default Services
---
+++ src/sections/index.js
@@ -0,0 +1,13 @@
+import Hero from './Hero.jsx';
+import Footer from './Footer';
+import CustomerReviews from './CustomerReviews';
+import PopularProducts from './PopularProducts';
+import Subscribe from './Subscribe';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import SuperQuality from './SuperQuality';
+
+
+export{
+    Hero, PopularProducts, Subscribe, SpecialOffer, SuperQuality, Footer, CustomerReviews, Services
+}
